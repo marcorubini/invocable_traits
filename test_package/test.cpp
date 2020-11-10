3139f10e610798e9511850e93fc633da39d62f8e
marcorubini <marco.rubini@studenti.unitn.it>
2020-11-11 00:18:00 +0100

    update test package

--- test_package/test.cpp
+++ test_package/test.cpp
@@ -1,7 +1,7 @@
 #include <ruby/invocable_traits/invocable_traits.hpp>
 #include <iostream>
 
-static_assert( inv::function_is_const_v<void() const> );
+static_assert( ruby::inv::function_is_const_v<void() const> );
 
 int main()
 {
